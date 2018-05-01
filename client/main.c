2d1adecd3740f60834c75161260c4b9b4a858317
rmurphy <ross.omurchadha@gmail.com>
2018-05-01 06:51:08 +0100

    logging test

--- client/main.c
+++ client/main.c
@@ -72,6 +72,8 @@ int main(int argc , char *argv[]) {
     // Login
     if (send(sock , username, strlen(username), 0) < 0) {
 //        memset(&username[0], 0, sizeof(username));
+        syslog(LOG_ERR, "Failed to send user to server.");
+        puts("Failed to send user to server. Exiting...");
         exit(EXIT_FAILURE);
     }
 //    memset(&username[0], 0, sizeof(username));
@@ -82,6 +84,7 @@ int main(int argc , char *argv[]) {
         exit(EXIT_FAILURE);
     }
     if (strcmp(server_reply, "OK") != 0) {
+        puts("Failed to send login");
         puts(server_reply);
         exit(EXIT_FAILURE);
     }
